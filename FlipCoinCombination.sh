793d36c16f15159c89e7e68b3b3d40fb85a08ac6
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 15:18:07 +0530

    added : added triplet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -54,3 +54,48 @@ echo "HT % = $(( (${doublet[HT]} * 100 ) / $dtotal))"
 echo "TH % = $(( (${doublet[TH]} * 100 ) / $dtotal))"
 echo "TT % = $(( (${doublet[TT]} * 100 ) / $dtotal))"
 }
+
+function tripletheadTailGenerate(){
+echo "------Triplet Combination-------"
+	while [ ${triplet[HHH]} -ne 21 -a ${triplet[HHT]} -ne 21 -a ${triplet[HTH]} -ne 21 -a ${triplet[THH]} -ne 21  -a ${triplet[TTH]} -ne 21  -a ${triplet[THT]} -ne 21  -a ${triplet[HTT]} -ne 21  -a ${triplet[TTT]} -ne 21 ]
+	do
+		rn1=$((RANDOM%2))
+		rn2=$((RANDOM%2))
+		rn3=$((RANDOM%2))
+       		if [ $rn1 -eq 0 -a $rn2 -eq 0 -a $rn3 -eq 0 ]
+		then
+			triplet[TTT]=$((triplet[TTT] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 0 -a $rn3 -eq 0 ]
+		then
+			triplet[HTT]=$((triplet[HTT] + 1))
+       		elif [ $rn1 -eq 0 -a $rn2 -eq 1 -a $rn3 -eq 0 ]
+		then
+			triplet[THT]=$((triplet[THT] + 1))
+       		elif [ $rn1 -eq 0 -a $rn2 -eq 0 -a $rn3 -eq 1 ]
+		then
+			triplet[TTH]=$((triplet[TTH] + 1))
+       		elif [ $rn1 -eq 0 -a $rn2 -eq 1 -a $rn3 -eq 1 ]
+		then
+			triplet[THH]=$((triplet[THH] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 0 -a $rn3 -eq 1 ]
+		then
+			triplet[HTH]=$((triplet[HTH] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 1 -a $rn3 -eq 0 ]
+		then
+			triplet[HHT]=$((triplet[HHT] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 1 -a $rn3 -eq 1 ]
+		then
+			triplet[HHH]=$((triplet[HHH] + 1))
+       		fi
+	done
+ttotal=$((triplet[HHH] + triplet[HHT] + triplet[HTH] + triplet[THH] + triplet[TTH] + triplet[THT] + triplet[HTT] + triplet[TTT]))
+echo "${triplet[@]} | total = $ttotal"
+echo "HHH % = $(( (${triplet[HHH]} * 100 ) / $ttotal))"
+echo "HHT % = $(( (${triplet[HHT]} * 100 ) / $ttotal))"
+echo "HTH % = $(( (${triplet[HTH]} * 100 ) / $ttotal))"
+echo "THH % = $(( (${triplet[THH]} * 100 ) / $ttotal))"
+echo "TTH % = $(( (${triplet[TTH]} * 100 ) / $ttotal))"
+echo "THT % = $(( (${triplet[THT]} * 100 ) / $ttotal))"
+echo "HTT % = $(( (${triplet[HTT]} * 100 ) / $ttotal))"
+echo "TTT % = $(( (${triplet[TTT]} * 100 ) / $ttotal))"
+}
