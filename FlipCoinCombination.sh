5706e9903cf0e08cb23d2e33b68cfb9cef4e84b0
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 15:11:35 +0530

    added : percentage program of heads and tails added

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -22,3 +22,35 @@ echo "------Singlet Combination-------"
 			singlet[H]=$((singlet[H] + 1))
        		fi
 	done
+echo "${singlet[@]} | total = $((singlet[H] + singlet[T]))"
+echo "H % = $(( (singlet[H] * 100 )/ (singlet[H] + singlet[T]) ))"
+echo "T % = $(( (singlet[T] * 100 )/ (singlet[H] + singlet[T]) ))"
+}
+
+function doubletheadTailGenerate(){
+echo "------Doublet Combination-------"
+	while [ ${doublet[HH]} -ne 21 -a ${doublet[HT]} -ne 21 -a ${doublet[TH]} -ne 21 -a ${doublet[TT]} -ne 21 ]
+	do
+		rn1=$((RANDOM%2))
+		rn2=$((RANDOM%2))
+       		if [ $rn1 -eq 0 -a $rn2 -eq 0 ]
+       		then
+			doublet[TT]=$((doublet[TT] + 1))
+       		elif [ $rn1 -eq 0 -a $rn2 -eq 1 ]
+		then
+			doublet[TH]=$((doublet[TH] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 0 ]
+		then
+			doublet[HT]=$((doublet[HT] + 1))
+       		elif [ $rn1 -eq 1 -a $rn2 -eq 1 ]
+		then
+			doublet[HH]=$((doublet[HH] + 1))
+       		fi
+	done
+dtotal=$((doublet[HH] + doublet[HT] + doublet[TH] + doublet[TT]))
+echo "${doublet[@]} | total = $dtotal"
+echo "HH % = $(( (${doublet[HH]} * 100 ) / $dtotal ))"
+echo "HT % = $(( (${doublet[HT]} * 100 ) / $dtotal))"
+echo "TH % = $(( (${doublet[TH]} * 100 ) / $dtotal))"
+echo "TT % = $(( (${doublet[TT]} * 100 ) / $dtotal))"
+}
